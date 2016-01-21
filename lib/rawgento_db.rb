8a0c3406b47688cfa8b3572c556083d397269521
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-01-21 08:42:59 +0100

    memoize settings and make configfile-path an optional parameter

--- lib/rawgento_db.rb
+++ lib/rawgento_db.rb
@@ -3,7 +3,9 @@ require "rawgento_db/query"
 require 'yaml'
 
 module RawgentoDB
-  def self.settings file: "rawgento_db-config.yml"
-    YAML.load_file file
+  DEFAULT_CONFIG_FILE = "rawgento_db-config.yml"
+  def self.settings file=DEFAULT_CONFIG_FILE
+    @@settings ||= YAML.load_file(file || DEFAULT_CONFIG_FILE)
+    @@settings
   end
 end
